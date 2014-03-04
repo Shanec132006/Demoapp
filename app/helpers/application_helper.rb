3b853336bc28849d267b844f755c011223df9dfe
Shanec132006 <Shanec132006@gmail.com>
2014-03-04 01:06:59 -0800

    Updated the helper code for application title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 	# Returns the full title on a per-page bases
 	def full_title(page_title)
 		base_title = "Ruby on Rails Tutorial Demo app"
-		if page.title.empty?
+		if page_title.empty?
 			base_title
 		else
 			"#{base_title} | #{page_title}"
